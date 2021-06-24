2ec288f87f3f38b3ac62f0e04e40ff3f2b206b08
Igor Barros <igorbarrosib@gmail.com>
2021-06-23 22:01:34 -0300

    Iniciando o projeto

---
+++ app/src/main/java/com/example/petlife/MainActivity.java
@@ -0,0 +1,120 @@
+package com.example.petlife;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import android.os.AsyncTask;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONStringer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button botaoRecuperar,buttonPorcaoUnica;
+    private JSONArray json;
+    private TextView textoResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MyTask taskTeste = new MyTask();
+        String url = "http://192.168.1.26:3000/horario/all";
+        taskTeste.execute(url);
+
+        botaoRecuperar = findViewById(R.id.buttonRecuperar);
+        buttonPorcaoUnica = findViewById(R.id.buttonPorcaoUnica);
+
+        textoResultado = findViewById(R.id.textResultado);
+
+        botaoRecuperar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyTask task = new MyTask();
+                String url = "http://192.168.1.26:3000/horario/2";
+                task.execute(url);
+
+            }
+        });
+
+
+        buttonPorcaoUnica.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyTask task = new MyTask();
+                String url = "http://192.168.15.5:3000/porcaoUnica/1";
+                task.execute(url);
+            }
+        });
+    }
+    class MyTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            String stringUrl = strings[0];
+            InputStream inputStream = null;
+            InputStreamReader inputStreamReader = null;
+            StringBuffer buffer = null;
+
+            try {
+
+                URL url = new URL(stringUrl);
+                HttpURLConnection conexao = (HttpURLConnection) url.openConnection();
+
+                // Recupera os dados em Bytes
+                inputStream = conexao.getInputStream();
+
+                //inputStreamReader lê os dados em Bytes e decodifica para caracteres
+                inputStreamReader = new InputStreamReader( inputStream );
+
+                //Objeto utilizado para leitura dos caracteres do InpuStreamReader
+                BufferedReader reader = new BufferedReader( inputStreamReader );
+                buffer = new StringBuffer();
+                String linha = "";
+
+                while((linha = reader.readLine()) != null){
+                    buffer.append( linha );
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return buffer.toString();//buffer.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String resultado) {
+            super.onPostExecute(resultado);
+            try {
+                json = new JSONArray(resultado);
+                //textoResultado.setText( json.getJSONObject(0).getString("hora") );
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
